e714a78bed20b96bf76f497d6816c7ba94f23299
Doug Nelson <doug@dochne.com>
2025-01-01 15:34:23 +0000

    change how a hrefs are made

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,16 +5,19 @@ interface Props {
   currentPost: Post;
 }
 
+function buildUrl(postId: string) {
+    const prefix = window.location.pathname.replace("post/", "")
+    return `${prefix}/post/${postId}`
+}
+
 export function Sidebar({ posts, currentPost }: Props) {
+
   return (
     <nav>
         <ul>
       {posts.map((post) => (
         <li className={post.cid == currentPost.cid ? "active" : ""} key={post.cid}>
-        <a
-          href={`/post/${post.uri.split("/").pop()}`}
-        >
-            {/* {post.indexedAt.split("T")[0]} - */}
+        <a href={buildUrl(post.uri.split("/").pop()!)}>
             {post.record.text}
         </a>
         </li>
